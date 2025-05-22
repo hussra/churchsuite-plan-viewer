dd3af1b6103cf98df1c32539162ff08ff82c6294
Richard Huss <richard@huss.org.uk>
2025-05-22 18:26:41 +0100

    Remove superfluous comments

--- src/renderer/preload-right.js
+++ src/renderer/preload-right.js
@@ -1,13 +1,4 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-
 import { contextBridge, ipcRenderer } from 'electron/renderer'
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     ipcRenderer.on('setPlan', (_event, value) => {
-//         alert(value)
-//     })
-// })
 
 contextBridge.exposeInMainWorld('electronAPI', {
     onSetPlan: (callback) => ipcRenderer.on('setPlan', (_event, value) => callback(value))
--- src/renderer/renderer-right.js
+++ src/renderer/renderer-right.js
@@ -1,39 +1,14 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
-
 import './right.css';
 import $ from 'jquery'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import { Liquid } from 'liquidjs'
-
-// const engine = new Liquid()
 
 window.electronAPI.onSetPlan((planDetail) => {
-    $('#planContents').html(planDetail)
+    if (planDetail != '') {
+        $('#selectPlan').hide()
+        $('#planContents').html(planDetail)
+    } else {
+        $('#selectPlan').show()
+        $('#planContents').html('')
+    }
 })
