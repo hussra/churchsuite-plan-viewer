bdde3ec0447424ab47651d5edf71d4682a29cd03
Richard Huss <richard@huss.org.uk>
2025-08-26 10:18:33 +0100

    Fix missing import causing inability to print 1-up

--- src/main/window-main.js
+++ src/main/window-main.js
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import * as fs from 'fs'
 import path from 'path'
 import { app, BaseWindow, BrowserWindow, dialog, Menu, nativeImage, screen, shell, WebContentsView } from 'electron'
 import coherentpdf from 'coherentpdf'
