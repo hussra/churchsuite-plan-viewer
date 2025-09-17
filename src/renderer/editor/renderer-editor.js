d9f7cea37823c82d184bd59964d8740f12f96fbf
Richard Huss <richard@huss.org.uk>
2025-09-17 19:12:27 +0100

    More buttons

--- src/renderer/editor/renderer-editor.js
+++ src/renderer/editor/renderer-editor.js
@@ -19,6 +19,7 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
+
 const populateTemplates = (templates) => {
 
     const templateSelect = document.getElementById('template')
@@ -46,6 +47,44 @@ const populateTemplates = (templates) => {
     templateSelect.dispatchEvent(new Event('change'))
 }
 
+
+const selectTemplate = async (templateId) => {
+    populateForm(await window.electronAPI.getFullTemplate(templateId))
+}
+
+const populateForm = (template) => {
+    if (template == null) {
+        document.getElementById('name').value = ''
+        document.getElementById('filenameSuffix').value = ''
+        document.getElementById('liquid').value = ''
+        document.getElementById('css').value = ''
+        document.getElementById('duplicateButton').setAttribute('disabled', 'disabled')        
+    } else {
+        document.getElementById('name').value = template.name
+        document.getElementById('filenameSuffix').value = template.filenameSuffix
+        document.getElementById('liquid').value = template.liquid
+        document.getElementById('css').value = template.css
+        document.getElementById('duplicateButton').removeAttribute('disabled')
+    }
+
+    if (template == null || !template.editable) {
+        document.getElementById('name').setAttribute('disabled', 'disabled')
+        document.getElementById('filenameSuffix').setAttribute('disabled', 'disabled')
+        document.getElementById('liquid').setAttribute('disabled', 'disabled')
+        document.getElementById('css').setAttribute('disabled', 'disabled')
+        document.getElementById('deleteButton').setAttribute('disabled', 'disabled')
+        document.getElementById('saveButton').setAttribute('disabled', 'disabled')
+    } else {
+        document.getElementById('name').removeAttribute('disabled')
+        document.getElementById('filenameSuffix').removeAttribute('disabled')
+        document.getElementById('liquid').removeAttribute('disabled')
+        document.getElementById('css').removeAttribute('disabled')
+        document.getElementById('deleteButton ').removeAttribute('disabled')
+        document.getElementById('saveButton ').removeAttribute('disabled')
+    }
+}
+
+
 const load = async () => {
     let templates = await window.electronAPI.getAllTemplates()
     populateTemplates(templates)
@@ -54,36 +93,27 @@ const load = async () => {
         const templateId = event.target.value
         await selectTemplate(templateId)
     })
+
+    document.getElementById('saveButton').addEventListener('click', async (event) => {
+        const templateId = document.getElementById('template').value
+        const name = document.getElementById('name').value
+        const filenameSuffix = document.getElementById('filenameSuffix').value
+        const liquid = document.getElementById('liquid').value
+        const css = document.getElementById('css').value
+
+        alert('Save not yet implemented')
+    })
+
+    document.getElementById('deleteButton').addEventListener('click', async (event) => {
+        alert('Delete not yet implemented')
+    })
+
+    document.getElementById('duplicateButton').addEventListener('click', async (event) => {
+        alert('Duplicate not yet implemented')
+    })
+
+    populateForm(null)
 }
 
-const selectTemplate = async (templateId) => {
-    let template = await window.electronAPI.getFullTemplate(templateId)
-
-    if (template == null) {
-        document.getElementById('name').value = ''
-        document.getElementById('filenameSuffix').value = ''
-        document.getElementById('liquid').value = ''
-        document.getElementById('css').value = ''
-    } else {
-        document.getElementById('name').value = template.name
-        document.getElementById('filenameSuffix').value = template.filenameSuffix
-        document.getElementById('liquid').value = template.liquid
-        document.getElementById('css').value = template.css
-    }
-
-    if (template == null || !template.editable) {
-        document.getElementById('name').setAttribute('disabled', 'disabled')
-        document.getElementById('filenameSuffix').setAttribute('disabled', 'disabled')
-        document.getElementById('liquid').setAttribute('disabled', 'disabled')
-        document.getElementById('css').setAttribute('disabled', 'disabled')
-        document.getElementById('saveButton').setAttribute('disabled', 'disabled')
-    } else {
-        document.getElementById('name').removeAttribute('disabled')
-        document.getElementById('filenameSuffix').removeAttribute('disabled')
-        document.getElementById('liquid').removeAttribute('disabled')
-        document.getElementById('css').removeAttribute('disabled')
-        document.getElementById('saveButton ').removeAttribute('disabled')
-    }
-}
 
 load()
