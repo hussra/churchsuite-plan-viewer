707e9175e9b5d5c3fcea24ee6b75eac6f57c68a7
Richard Huss <richard@huss.org.uk>
2026-02-05 20:46:40 +0000

    On load, use on-disk OOTB templates to update settings

--- src/main/template-engine.js
+++ src/main/template-engine.js
@@ -64,6 +64,22 @@ export class TemplateEngine {
                         liquid: this.#getTemplateLiquidFromDisk(basename),
                         css: this.#getTemplateCSSFromDisk(basename)
                     })
+
+                    // Load into settings if not already present, so that template settings can be edited by the user and saved against the template
+                    const tmpl = {
+                        name: jsonData.name,
+                        filenameSuffix: jsonData.filenameSuffix,
+                        liquid: this.#getTemplateLiquidFromDisk(basename),
+                        css: this.#getTemplateCSSFromDisk(basename),
+                        font_size: (jsonData.font_size ? jsonData.font_size : this.#controller.getGlobalSetting(`templates.${basename}.font_size`)),
+                        name_style: (jsonData.name_style ? jsonData.name_style : this.#controller.getGlobalSetting(`templates.${basename}.name_style`)),
+                        song_lyrics: (jsonData.song_lyrics ? jsonData.song_lyrics : this.#controller.getGlobalSetting(`templates.${basename}.song_lyrics`)),
+                        page_size: (jsonData.page_size ? jsonData.page_size : this.#controller.getGlobalSetting(`templates.${basename}.page_size`)),
+                        two_up: (jsonData.two_up ? jsonData.two_up : this.#controller.getGlobalSetting(`templates.${basename}.two_up`)),
+                        page_numbers: (jsonData.page_numbers ? jsonData.page_numbers : this.#controller.getGlobalSetting(`templates.${basename}.page_numbers`)),
+                    }
+
+                    this.#controller.setGlobalSetting(`templates.${basename}`, tmpl)
                 } catch (err) {
                     console.warn('Skipping template ' + basename + ' due to ' + err.message)
                 }
