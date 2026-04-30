48aa2d7b8c2aac91ead38ac778bd9543de7d8fdf
Richard Huss <richard@huss.org.uk>
2026-04-30 13:06:51 +0100

    Add eslint - config not yet perfect, getting errors for webpack entries, but enough to help me fix stuff up.

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+import { glob } from "original-fs";
+
+const defaultConfig = {
+  plugins: { js },
+  rules: { 'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_(.+)', caughtErrorsIgnorePattern: '^_' }], },
+  extends: ["js/recommended"],
+  languageOptions: { globals: globals.nodeBuiltin }  
+}
+
+export default defineConfig([
+  { ...defaultConfig, files: ["src/main/**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.nodeBuiltin } },
+  { ...defaultConfig, files: ["src/renderer/**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  globalIgnores([
+    "dist/**/*",
+    "node_modules/**/*",
+    "out/**/*",
+    ".webpack/**/*",
+    ".yarn/**/*",
+  ]),
+]);
