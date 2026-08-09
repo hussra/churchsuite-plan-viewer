b96ec21e8724a4407150a738908dcfdbd876554b
Richard Huss <richard@huss.org.uk>
2026-08-09 19:37:22 +0100

    Add macOS .dmg maker

--- forge.config.js
+++ forge.config.js
@@ -38,6 +38,23 @@ module.exports = {
       platforms: ['darwin'],
     },
     {
+      name: '@electron-forge/maker-dmg',
+      config: (arch) => ({
+        background: './assets/background-DMG.png',
+        format: 'ULFO',
+        iconSize: 200,
+        contents: [
+        {
+          x: 140,
+          y: 276,
+          type: "file",
+          path: `${process.cwd()}/out/ChurchSuite Plan Viewer-darwin-${arch}/ChurchSuite Plan Viewer.app`,
+        },
+        { x: 518, y: 276, type: "link", path: "/Applications" },
+      ]
+      })
+    },
+    {
       name: '@electron-forge/maker-deb',
       config: {},
     },
